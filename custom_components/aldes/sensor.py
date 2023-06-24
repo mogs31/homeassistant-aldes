a449f1ee4131937b09e57fce61136b86687fa5af
Fredzxda <37531742+Fredzxda@users.noreply.github.com>
2023-06-24 08:26:36 +0200

    Solve Thermostats sensor not well implemented + other corrections

--- custom_components/aldes/sensor.py
+++ custom_components/aldes/sensor.py
@@ -16,7 +16,7 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN, FRIENDLY_NAMES
+from .const import DOMAIN, FRIENDLY_NAMES, POLLUANTS
 from .entity import AldesEntity
 
 from collections.abc import Callable
@@ -24,6 +24,7 @@ from dataclasses import dataclass
 
 ATTR_HUMIDITY = "humidity"
 ATTR_TEMPERATURE = "temperature"
+ATTR_THERMOSTAT = "thermostat"
 ATTR_CO2 = "CO2"
 ATTR_QAI = "Air Quality Index"
 ATTR_POLLUANT = "Polluant Dominant"
@@ -41,7 +42,9 @@ class AldesSensorDescription(SensorEntityDescription):
     path1: str = None
     path2: str = None
     path3: str = None
-    divisor: int = None
+    path2recursive: bool = False
+    path2id: str = None
+    path2value: str = None
 
 
 EASY_HOME_SENSORS = {
@@ -66,7 +69,7 @@ EASY_HOME_SENSORS = {
         entity_category=EntityCategory.DIAGNOSTIC,
         path1="indicator",
         path2="TmpCu",
-        divisor=10,
+        value=lambda value: value / 10,
     ),
     f"Bathroom_1_{ATTR_HUMIDITY}": AldesSensorDescription(
         key="status",
@@ -89,7 +92,7 @@ EASY_HOME_SENSORS = {
         entity_category=EntityCategory.DIAGNOSTIC,
         path1="indicator",
         path2="TmpBa1",
-        divisor=10,
+        value=lambda value: value / 10,
     ),
     f"Bathroom_2_{ATTR_HUMIDITY}": AldesSensorDescription(
         key="status",
@@ -112,7 +115,7 @@ EASY_HOME_SENSORS = {
         entity_category=EntityCategory.DIAGNOSTIC,
         path1="indicator",
         path2="TmpBa2",
-        divisor=10,
+        value=lambda value: value / 10,
     ),
     f"{ATTR_CO2}": AldesSensorDescription(
         key="status",
@@ -147,6 +150,7 @@ EASY_HOME_SENSORS = {
         path1="indicator",
         path2="Qai",
         path3="polluantDominant",
+        value=lambda value: POLLUANTS[value],
     ),
     f"{ATTR_VARHR}": AldesSensorDescription(
         key="status",
@@ -172,6 +176,24 @@ EASY_HOME_SENSORS = {
     ),
 }
 
+TONE_AIR_SENSORS = {
+    f"{ATTR_THERMOSTAT}": AldesSensorDescription(
+        key="status",
+        icon="mdi:thermometer",
+        name="Thermostat",
+        translation_key="temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        path1="indicator",
+        path2="thermostats",
+        path2recursive=True,
+        path2id="ThermostatId",
+        path2value="CurrentTemperature",
+        value=lambda value: round(value, 1),
+    ),
+}
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -180,20 +202,37 @@ async def async_setup_entry(
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     entities: list[AldesSensorEntity] = []
-    sensors = EASY_HOME_SENSORS
     for product in coordinator.data:
+        if product["reference"] == "EASY_HOME_CONNECT":
+            sensors = EASY_HOME_SENSORS
+        elif product["reference"] == "TONE_AIR":
+            sensors = TONE_AIR_SENSORS
         for sensor, description in sensors.items():
-            entities.append(
-                AldesSensorEntity(
-                    coordinator,
-                    entry,
-                    product["serial_number"],
-                    product["reference"],
-                    product["modem"],
-                    sensor,
-                    description,
+            if description.path2recursive:
+                for thermostat in product[description.path1][description.path2]:
+                    entities.append(
+                        AldesSensorEntity(
+                            coordinator,
+                            entry,
+                            product["serial_number"],
+                            product["reference"],
+                            product["modem"],
+                            thermostat["ThermostatId"],
+                            description,
+                        )
+                    )
+            else:
+                entities.append(
+                    AldesSensorEntity(
+                        coordinator,
+                        entry,
+                        product["serial_number"],
+                        product["reference"],
+                        product["modem"],
+                        sensor,
+                        description,
+                    )
                 )
-            )
 
     async_add_entities(entities)
 
@@ -218,19 +257,9 @@ class AldesSensorEntity(AldesEntity, SensorEntity):
         self.entity_description = description
         self._attr_native_value = self._determine_native_value()
 
-    # @property
-    # def device_info(self):
-    #    """Return the device info."""
-    #    return DeviceInfo(
-    #        identifiers={(DOMAIN, self.product_serial_number)},
-    #        name=f"{FRIENDLY_NAMES[self.reference]} {self.product_serial_number}",
-    #        model=FRIENDLY_NAMES[self.reference],
-    #    )
-
     @property
     def unique_id(self):
         """Return a unique ID to use for this entity."""
-        # return f"{DOMAIN}_{self.product_serial_number}_{self.entity_description.name}"
         return f"{FRIENDLY_NAMES[self.reference]}_{self.product_serial_number}_{self.entity_description.name}"
 
     @property
@@ -238,7 +267,12 @@ class AldesSensorEntity(AldesEntity, SensorEntity):
         """Return a name to use for this entity."""
         for product in self.coordinator.data:
             if product["serial_number"] == self.product_serial_number:
-                return f"{FRIENDLY_NAMES[self.reference]} {self.product_serial_number} {self.entity_description.name}"
+                if self.entity_description.path2 == "thermostats":
+                    for thermostat in product["indicator"]["thermostats"]:
+                        if thermostat["ThermostatId"] == self.probe_id:
+                            return f"{FRIENDLY_NAMES[self.reference]} {self.product_serial_number} {thermostat['Name']} temperature"
+                else:
+                    return f"{FRIENDLY_NAMES[self.reference]} {self.product_serial_number} {self.entity_description.name}"
             return None
 
     def _determine_native_value(self):
@@ -246,7 +280,16 @@ class AldesSensorEntity(AldesEntity, SensorEntity):
         for product in self.coordinator.data:
             if product["isConnected"]:
                 if product["serial_number"] == self.product_serial_number:
-                    if self.entity_description.path3 is None:
+                    if self.entity_description.path2recursive:
+                        for thermostat in product[self.entity_description.path1][
+                            self.entity_description.path2
+                        ]:
+                            if (
+                                thermostat[self.entity_description.path2id]
+                                == self.probe_id
+                            ):
+                                value = thermostat[self.entity_description.path2value]
+                    elif self.entity_description.path3 is None:
                         value = product[self.entity_description.path1][
                             self.entity_description.path2
                         ]
@@ -254,14 +297,15 @@ class AldesSensorEntity(AldesEntity, SensorEntity):
                         value = product[self.entity_description.path1][
                             self.entity_description.path2
                         ][self.entity_description.path3]
-                    if self.entity_description.divisor is None:
-                        return value
-                    else:
-                        return value / self.entity_description.divisor
                 else:
-                    return None
+                    value = None
             else:
-                return None
+                value = None
+
+        if value is not None:
+            if self.entity_description.value:
+                value = self.entity_description.value(value)
+        return value
 
     @callback
     def _handle_coordinator_update(self):
